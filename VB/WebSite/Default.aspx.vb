765d9117f8401c7b4f502792c3824a4937a5fb3b
DevExpressExampleBot <fakeEmail>
2026-01-29 20:35:35 +0400

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Web
@@ -8,6 +7,7 @@ Imports System.Web.UI.WebControls
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
+
 	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
 
 	End Sub
